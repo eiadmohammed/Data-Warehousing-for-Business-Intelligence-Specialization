7e59f19bf68d08de94858ac7981cf1620535f3e9
Eiad Mohammed <eiad.deyab@gmail.com>
2025-04-24 17:00:28 +0300

    Update schema and queries
    update contain the relation between tables and missed queries and uncorrect codes

--- droptables.sql
+++ droptables.sql
@@ -2,7 +2,7 @@ DROP TABLE 'Customer'
 ;
 DROP TABLE 'CustLocation'
 ;
-DROP TABLE 'Invoices'
+DROP TABLE 'Invoice'
 ;
 DROP TABLE 'Job'
 ;
@@ -16,5 +16,12 @@ DROP TABLE 'MachineType'
 ;
 DROP TABLE 'SalesAgent'
 ;
-DROP TABLE 'SalesClass' 
+DROP TABLE 'SalesClass'
 ;
+DROP TABLE 'FinancialSalesSummary'
+;
+DROP TABLE 'FinancialCostSummary'
+;
+DROP TABLE 'LeadFile' 
+;
+
--- schema.sql
+++ schema.sql
@@ -4,9 +4,11 @@
 
 -- Dimension Table
 -- CREATE DATABASE `DATAWAREHOUSINGFORBUSINESSFINALPROJECT`;
--- USE DATAWAREHOUSINGFORBUSINESSFINALPROJECT
-;
+-- USE DATAWAREHOUSINGFORBUSINESSFINALPROJECT ;
 
+
+-- Deminsion Table
+-- this table has two hierarchicals (country, state, city), (country, zipcode)
 CREATE TABLE 'Customer'
 (
     "Cust_key" BIGINT Primary Key  ,-- Primary key
@@ -15,19 +17,79 @@ CREATE TABLE 'Customer'
     "Address_1" varchar Not Null ,
     "Address_2" varchar ,
     "Address_3" varchar  ,
-    "City" varchar NOT NULL ,
     "Country" varchar ,
+    "State" char(2) NOT NULL ,
+    "City" varchar NOT NULL ,
+    "ZIP" char(10),
     "Credit_Limit" numeric  ,
     "Date_First_Order" date NOT NULL ,
     "E_Mail_Address" varchar NOT NULL ,
     "Fax" char(10) ,
     "Phone" char(10) NOT NULL ,
     "Sales_Tax_Code" char(10)  ,
-    "State" char(2) NOT NULL ,
-    "Terms_Code" char(10)  ,
-    "ZIP" char(10)
+    "Terms_Code" char(10)
 
 );
+
+
+CREATE TABLE 'Shipment'
+(
+    "Shipment_Id" BIGINT PRIMARY KEY,
+    "Actual_Ship_Date" date ,
+    "Requested_Ship_Date" date ,
+    "Actual_Quantity" int ,
+    "Requested_Quantity" int ,
+    "Boxes" int ,
+    "Quantity_Per_Box" int ,
+    "Quantity_Per_Partial_Box" int ,
+    "Job_Id" BIGINT ,
+    "Shipment_Amount" numeric ,
+    "Sub_Job_Id" BIGINT ,
+    "Invoice_Id" BIGINT ,
+    "Cust_Loc_Key" BIGINT,
+    FOREIGN KEY (Job_Id) REFERENCES Job(Job_Id) ,
+    FOREIGN KEY (Sub_Job_Id) REFERENCES SubJob(Sub_Job_Id),
+    FOREIGN KEY (Invoice_Id) REFERENCES Invoice(Invoice_Id),
+    FOREIGN KEY (Cust_Loc_Key) REFERENCES CustLocation(Cust_Loc_Key)
+);
+
+-- Deminsion Table
+CREATE TABLE 'Location'
+(
+    "Location_Id" BIGINT PRIMARY KEY ,
+    "Location_Name" char(50)
+
+);
+
+-- Deminsion Table
+CREATE TABLE 'MachineType'
+(
+    "Machine_Type_Id" BIGINT PRIMARY KEY,
+    "Manufacturer" BIGINT ,
+    "Model" varchar ,
+    "Rate_Per_Hour" numeric ,
+    "Number_Of_Machines" int
+);
+
+-- Deminsion Table
+CREATE TABLE 'SalesAgent'
+(
+    "Sales_Agent_Id" BIGINT PRIMARY KEY ,
+    "Sales_Agent_Name" varchar ,
+    "State" char(2) ,
+    "Country" char(25) ,
+    "Record_Active" boolean
+
+);
+
+-- Deminsion Table
+CREATE TABLE 'SalesClass'
+(
+    "Sales_Class_Id" BIGINT Primary key,
+    "Sales_Class_Desc" varchar
+
+);
+
 
 -- Deminsion Table
 CREATE TABLE 'CustLocation'
@@ -38,23 +100,24 @@ CREATE TABLE 'CustLocation'
     "Address_1" varchar ,
     "Address_2" varchar ,
     "Address_3" varchar ,
-    "City" varchar ,
     "Country" varchar ,
+    "State" char(2) ,
+    "City" varchar ,
+    "ZIP" char(10) ,
     "Credot_limit" numeric ,
     "E_Mail_Address" varchar ,
     "Fax" char(10) ,
     "Phone" char(10) ,
     "Sales_Tax_Cod" char(10) ,
-    "State" char(2) ,
     "Terms_Code" char(10) ,
-    "ZIP" char(10) ,
-    "Cust_key" BIGINT
+    "Cust_key" BIGINT ,
+    FOREIGN KEY (Cust_Key) REFERENCES Customer(Cust_Key)
 
 
 );
 
--- Fact Table
-CREATE TABLE 'Invoices'
+-- Deminsion Table
+CREATE TABLE 'Invoice'
 (
     "Invoice_Id" BIGINT Primary Key ,
     "Date_Invoiced" date ,
@@ -64,11 +127,35 @@ CREATE TABLE 'Invoices'
     "Invoice_Amount" numeric  ,
     "Invoice_Quantity" int  ,
     "Invoice_Shipped" int  ,
-    "Cust_Key" BIGINT
+    "Cust_Key" BIGINT,
+    FOREIGN KEY (Cust_Key) REFERENCES Customer(Cust_Key)
 
 );
 
 
+-- Deminsion Table
+CREATE TABLE 'SubJob'
+(
+    "Job_Id" BIGINT ,
+    "Sub_Job_Id" tinyint ,
+    "Sub_Job_Desc" char(50) ,
+    "Cost_Labor" numeric ,
+    "Cost_Material" numeric ,
+    "Cost_Overhead" numeric ,
+    "Machine_Hours" decimal(10,2) ,
+    "Date_Prod_Begin" date ,
+    "Date_Prod_End" date ,
+    "Quantity_Produced" int ,
+    "Sub_Job_Amount" numeric ,
+    "Machine_Type_Id" BIGINT ,
+    PRIMARY KEY (Job_Id, Sub_Job_Id) ,
+    FOREIGN KEY (Job_Id) REFERENCES Job(Job_Id) ,
+    FOREIGN KEY (Machine_Type_Id) REFERENCES MachineType(Machine_Type_Id)
+
+);
+
+
+-- Deminsion Table
 CREATE TABLE 'Job'
 (
     "Job_Id" BIGINT Primary Key ,
@@ -87,85 +174,89 @@ CREATE TABLE 'Job'
     "Quotation_Amount" numeric ,
     "Quotation_Ordered" tinyint  ,
     "Location_Id" BIGINT ,
-    "ales_Class_Id" BIGINT ,
+    "Sales_Class_Id" BIGINT ,
     "Sales_Agent_Id" BIGINT ,
-    "Cust_Key" BIGINT
+    "Cust_Key" BIGINT ,
+    FOREIGN KEY (Cust_Key) REFERENCES Customer(Cust_Key) ,
+    FOREIGN KEY (Sales_Agent_Id) REFERENCES SalesAgent(Sales_Agent_Id) ,
+    FOREIGN KEY (Location_Id) REFERENCES Location(Location_Id) ,
+    FOREIGN KEY (Sales_Class_Id) REFERENCES SalesClass(Sales_Class_Id)
 
 );
 
-
-CREATE TABLE 'SubJob'
+CREATE TABLE 'FinancialSalesSummary'
 (
-    "Job_Id" BIGINT ,
-    "Sub_Job_Id" tinyint ,
-    "Sub_Job_Desc" char(50) ,
-    "Cost_Labor" numeric ,
-    "Cost_Material" numeric ,
-    "Cost_Overhead" numeric ,
-    "Machine_Hours" decimal(10,2) ,
-    "Date_Prod_Begin" date ,
-    "Date_Prod_En" date ,
-    "Quantity_Produced" int ,
-    "Sub_Job_Amount" numeric ,
-    "Machine_Type_Id" BIGINT
-
-);
-
--- Dimension Table
-CREATE TABLE 'Shipment'
-(
-    "Shipment_Id" BIGINT ,
-    "Actual_Ship_Date" date ,
-    "Requested_Ship_Date" date ,
-    "Actual_Quantity" int ,
-    "Requested_Quantity" int ,
-    "Boxes" int ,
-    "Quantity_Per_Box" int ,
-    "Quantity_Per_Partial_Box" int ,
-    "Job_Id" BIGINT ,
-    "Shipment_Amount" numeric ,
-    "Sub_Job_Id" BIGINT ,
-    "Invoice_Id" BIGINT ,
-    "Cust_Loc_Key" BIGINT
-
-);
-
-
-CREATE TABLE 'Location'
-(
+    "Summary_Sales_Id" BIGINT Primary KEY ,
+    "Actual_Units" INTEGER ,
+    "Actual_Amount" numeric ,
+    "Forecast_Units" INTEGER ,
+    "Forecast_Amount" numeric ,
     "Location_Id" BIGINT ,
-    "Location_Name" char(50)
-
+    "Sales_Class_Id" BIGINT ,
+    "Begin_Date" Date ,
+    "End_Date" Date,
+    FOREIGN KEY (Sales_Class_Id) REFERENCES SalesClass(Sales_Class_Id),
+    FOREIGN KEY (Location_Id) REFERENCES Location(Location_Id)
 );
 
-
-CREATE TABLE 'MachineType'
+CREATE TABLE 'FinancialCostSummary'
 (
+    "Summary_Cost_Id" BIGINT PRIMARY KEY ,
+    "Actual_Units" INTEGER ,
+    "Actual_Labor_Cost" numeric ,
+    "Actual_Material_Cost" numeric ,
+    "Actual_Machine_Cost" numeric ,
+    "Actual_Overhead_Cost" numeric ,
+    "Budget_Units" INTEGER ,
+    "Budget_Labor_Cost" NUMERIC ,
+    "Budget_Material_Cost" numeric ,
+    "Budget_Machine_Cost" numeric ,
+    "Budget_Overhead_Cos" numeric ,
+    "Location_Id" BIGINT ,
     "Machine_Type_Id" BIGINT ,
-    "Manufacturer" BIGINT ,
-    "Model" varchar ,
-    "Rate_Per_Hour" numeric ,
-    "Number_Of_Machines" int
+    "Sales_Class_Id" BIGINT ,
+    "Begin_Date" date ,
+    "End_Date" date,
+    FOREIGN KEY (Machine_Type_Id) REFERENCES MachineType(Machine_Type_Id),
+    FOREIGN KEY (Sales_Class_Id) REFERENCES SalesClass(Sales_Class_Id),
+    FOREIGN KEY (Location_Id) REFERENCES Location(Location_Id)
 
 );
 
-
-CREATE TABLE 'SalesAgent'
+CREATE TABLE 'LeadFile'
 (
-    "Sales_Agent_Id" BIGINT
-    "Sales_Agent_Name" varchar ,
-"State" char
-    (2) ,
-"Country" char
-    (25) ,
-"Record_Active" boolean
-
+    "Lead_Id" BIGINT PRIMARY Key ,
+    "Quote_Qty" INTEGER ,
+    "Quote_Price" NUMERIC ,
+    "Quote_Value" NUMERIC ,
+    "Success" boolean ,
+    "PO_Number" Char(10) ,
+    "Created_Date" Date ,
+    "Cust_Key" BIGINT ,
+    "Location_Id" BIGINT ,
+    "Sales_Agent_Id" BIGINT  ,
+    "Sales_Class_Id" BIGINT ,
+    FOREIGN KEY (PO_Number) REFERENCES Job(PO_Number) ,
+    FOREIGN KEY (Cust_Key) REFERENCES Customer(Cust_Key) ,
+    FOREIGN KEY (Location_Id) REFERENCES Location(Location_Id) ,
+    FOREIGN KEY (Sales_Agent_Id) REFERENCES SalesAgent(Sales_Agent_Id),
+    FOREIGN KEY (Sales_Class_Id) REFERENCES SalesClass(Sales_Class_Id)
 );
 
 
-    CREATE TABLE 'SalesClass'
-    (
-        "Sales_Class_Id" BIGINT ,
-        "sales_class_Desc" varchar
 
-    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
